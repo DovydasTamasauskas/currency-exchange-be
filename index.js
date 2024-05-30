740c0adffb3eab59a8cece3496494b05dcddd7a2
DovydasT <tamasauskas.dovydas@gmail.com>
2024-05-30 10:59:48 +0300

    query params validation

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const server = require("./public/server");
 const { EXCHANGE_URL } = require("./public/urls");
 
 const app = server.init();
+const SUPPORTED_CURRENCIES = ["USD", "EUR", "GBP", "ILS"];
 
 app.get("/", async (req, res) => {
   res.send("working...");
@@ -12,16 +13,31 @@ app.get("/exchange", async (req, res) => {
   const { baseCurrency, quoteCurrency, baseAmount } = req.query;
   var rates = null;
 
+  if (
+    !baseCurrency ||
+    !quoteCurrency ||
+    !SUPPORTED_CURRENCIES.includes(baseCurrency) ||
+    !SUPPORTED_CURRENCIES.includes(quoteCurrency)
+  )
+    return res.status(400).json({
+      error: "Currency is not supported by system",
+    });
+
+  if (!baseAmount || isNaN(baseAmount)) {
+    return res.status(400).json({ error: "Excahnge amount is incorrect" });
+  }
+
   if (baseCurrency && quoteCurrency && baseAmount) {
     try {
       const response = await axios.get(EXCHANGE_URL);
       rates = response.data.rates;
-    } catch (error) {
-      return res
-        .status(500)
-        .json({ message: "Failed to fetch exchange rates", error });
+    } catch {
+      return res.status(500).json({ error: "Failed to fetch" });
     }
   }
+
+  if (!rates[quoteCurrency] || !rates[baseCurrency])
+    return res.status(400).json({ error: "Currency not supported" });
 
   res.json(rates);
 });
