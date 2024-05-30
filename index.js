8b940f30960c9bdcbc4f047dbe5dca8b718f69ef
DovydasT <tamasauskas.dovydas@gmail.com>
2024-05-30 21:58:56 +0300

    clean up

--- index.js
+++ index.js
@@ -27,23 +27,21 @@ app.get("/exchange", async (req, res) => {
     return res.status(400).json({ error: "Excahnge amount is incorrect" });
   }
 
-  if (baseCurrency && quoteCurrency && baseAmount) {
-    try {
-      rates = cache.get(baseCurrency);
-      if (!rates) {
-        const response = await axios.get(`${EXCHANGE_URL}/${baseCurrency}`);
-        rates = response.data.rates;
-        cache.set(baseCurrency, rates);
-      }
-    } catch {
-      return res.status(500).json({ error: "Failed to fetch" });
+  rates = cache.get(baseCurrency);
+  try {
+    if (!rates) {
+      const response = await axios.get(`${EXCHANGE_URL}/${baseCurrency}`);
+      rates = response.data.rates;
+      cache.set(baseCurrency, rates);
     }
+  } catch {
+    return res.status(500).json({ error: "Failed to fetch" });
   }
 
   if (!rates[quoteCurrency] || !rates[baseCurrency])
     return res.status(400).json({ error: "Currency not supported" });
 
-  const result = Math.round(baseAmount * rates[quoteCurrency] * 10) / 1000;
+  const result = Math.round(baseAmount * rates[quoteCurrency] * 10) / 10; // to get extra decimal place
 
   res.json({ result });
 });
