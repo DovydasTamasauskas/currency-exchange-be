c53f72be9a243858a20edd21489bfe933ec99032
DovydasT <tamasauskas.dovydas@gmail.com>
2024-05-30 14:51:44 +0300

    added lru cach

---
+++ LRUCache.js
@@ -0,0 +1,24 @@
+class LRUCache {
+  constructor(max = 10) {
+    this.max = max;
+    this.cache = new Map();
+  }
+
+  get(key) {
+    return this.cache.get(key);
+  }
+
+  set(key, val) {
+    this.cache.set(key, val);
+  }
+
+  first() {
+    return this.cache.keys().next().value;
+  }
+
+  getAll() {
+    return this.cache.keys();
+  }
+}
+
+module.exports = { LRUCache };
--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 const axios = require("axios");
 const server = require("./public/server");
 const { EXCHANGE_URL } = require("./public/urls");
+const { LRUCache } = require("./LRUCache");
 
 const app = server.init();
+
 const SUPPORTED_CURRENCIES = ["USD", "EUR", "GBP", "ILS"];
 
-app.get("/", async (req, res) => {
-  res.send("working...");
-});
-
-app.get("/get/supported-currencies", async (req, res) => {
-  res.json({ currencies: SUPPORTED_CURRENCIES });
-});
+const cache = new LRUCache(SUPPORTED_CURRENCIES.length);
 
 app.get("/exchange", async (req, res) => {
   const { baseCurrency, quoteCurrency, baseAmount } = req.query;
@@ -33,8 +29,12 @@ app.get("/exchange", async (req, res) => {
 
   if (baseCurrency && quoteCurrency && baseAmount) {
     try {
-      const response = await axios.get(`${EXCHANGE_URL}/${baseCurrency}`);
-      rates = response.data.rates;
+      rates = cache.get(baseCurrency);
+      if (!rates) {
+        const response = await axios.get(`${EXCHANGE_URL}/${baseCurrency}`);
+        rates = response.data.rates;
+        cache.set(baseCurrency, rates);
+      }
     } catch {
       return res.status(500).json({ error: "Failed to fetch" });
     }
@@ -43,7 +43,11 @@ app.get("/exchange", async (req, res) => {
   if (!rates[quoteCurrency] || !rates[baseCurrency])
     return res.status(400).json({ error: "Currency not supported" });
 
-  const quoteAmount = Math.round(baseAmount * rates[quoteCurrency] * 10) / 1000;
+  const result = Math.round(baseAmount * rates[quoteCurrency] * 10) / 1000;
 
-  res.json({ quoteAmount, rates });
+  res.json({ result });
+});
+
+app.get("/get/supported-currencies", async (req, res) => {
+  res.json({ currencies: SUPPORTED_CURRENCIES });
 });
