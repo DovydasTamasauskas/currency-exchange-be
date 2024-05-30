57f05b001767206ae28b58afad3dc0dfe2ad58d3
DovydasT <tamasauskas.dovydas@gmail.com>
2024-05-30 09:57:00 +0300

    exchange endpiont call

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require("express");
+const axios = require("axios");
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
+const EXCHANGE_URL = "https://api.exchangerate-api.com/v4/latest/USD";
 
 const app = express();
 
 app.get("/", async (req, res) => {
   res.send("working...");
+});
+
+app.get("/exchange", async (req, res) => {
+  const { baseCurrency, quoteCurrency, baseAmount } = req.query;
+  var result = null;
+  if (baseCurrency && quoteCurrency && baseAmount) {
+    const response = await axios.get(EXCHANGE_URL);
+    result = response.data.rates;
+  }
+
+  res.json(result);
 });
 
 app.listen(PORT, () => {
